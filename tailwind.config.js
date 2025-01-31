8bbc739c6f86888fb8a3f06fb142afab24aa2860
Galib <assadulla0172@gmail.com>
2025-02-01 01:49:08 +0600

    animate the header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
         'navbtn' : 'rgb(23, 152, 0)',
         'gradianfrom' : 'rgb(255, 255, 255)',
         'gradianto' : 'rgb(239, 235, 227)',
+      },
+      animation: {
+        cust: 'cust 5s ease-in-out infinite',
+      },
+      keyframes: {
+        cust: {
+          '0%, 100%': { transform: 'scale(1)', opacity: '0.8' },
+          '50%': { transform: 'scale(1.1)', opacity: '1' },
+        }
       }
     }
   },
