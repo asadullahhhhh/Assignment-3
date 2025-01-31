ed2163e18de48a637cae3ac0451dbf979660125a
Galib <assadulla0172@gmail.com>
2025-02-01 00:40:31 +0600

    Creati the navbar and it is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['*'],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sora': ["Sora", "serif"],
+    },
+    extend: {
+      colors: {
+        'navItemsColor' : 'rgb(17, 17, 17)',
+        'navbtn' : 'rgb(23, 152, 0)',
+        'gradianfrom' : 'rgb(255, 255, 255)',
+        'gradianto' : 'rgb(239, 235, 227)',
+      }
+    }
   },
   plugins: [],
 }
